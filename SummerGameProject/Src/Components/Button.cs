98da8d0e995dd28a1eb67dc96ff8866bb9a0964b
Rob-Turff <robert_turff@yahoo.com>
2019-07-26 22:20:13 +0100

    added button componant

---
+++ SummerGameProject/Src/Components/Button.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace SummerGameProject.Src.Components
+{
+    class Button : Component
+    {
+        public int ButtonX { get; set; }
+        public int ButtonY { get; set; }
+
+        public Action OnClick { get; set; }
+
+        private string text;
+        private Texture2D texture;
+        private MouseState oldMouse;
+        private MouseState currentMouse;
+        private Color colour;
+        private SpriteFont font;
+
+        public Button(string text, Texture2D texture, int ButtonX, int ButtonY, SpriteFont font)
+        {
+            this.text = text;
+            this.texture = texture;
+            this.ButtonX = ButtonX;
+            this.ButtonY = ButtonY;
+            this.font = font;
+        }
+
+        public override void Draw(GameTime gameTime, SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(texture, new Vector2(ButtonX, ButtonY), colour);
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            colour = Color.White;
+            oldMouse = currentMouse;
+            currentMouse = Mouse.GetState();
+            if (currentMouse.X < ButtonX + texture.Width && currentMouse.X > ButtonX && currentMouse.Y < ButtonY + texture.Height && currentMouse.Y > ButtonY)
+            {
+                colour = Color.Yellow;
+                if (currentMouse.LeftButton == ButtonState.Released && oldMouse.LeftButton == ButtonState.Pressed)
+                {
+                    OnClick?.Invoke();
+                }
+            }
+
+
+        }
+    }
+}
