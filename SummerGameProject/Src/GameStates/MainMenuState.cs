696d83d8791313821a6a38cbe79c3612bff39d42
rxt711 <rxt711@cs.bham.ac.uk>
2019-07-26 15:26:13 +0100

    added src folder

---
+++ SummerGameProject/Src/GameStates/MainMenuState.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace SummerGameProject.Src.GameStates
+{
+    public class MainMenuState : State
+    {
+        public override void Draw(GameTime gameTime, SpriteBatch spriteBatch)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ SummerGameProject/Src/GameStates/State.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SummerGameProject.Src.GameStates
+{
+    /// <summary>
+    /// Abstract class to create game states e.g. main menu
+    /// </summary>
+    public abstract class State
+    {
+        #region Fields
+        #endregion
+
+        #region Methods
+        public abstract void Draw(GameTime gameTime, SpriteBatch spriteBatch);
+
+        public abstract void Update(GameTime gameTime)
+        #endregion
+    }
+}
