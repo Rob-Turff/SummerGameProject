46998b8272021f2550fa61026e62701aa121712d
Rob-Turff <robert_turff@yahoo.com>
2019-07-29 15:11:58 +0100

    added settings button and state

---
+++ SummerGameProject/Src/GameStates/SettingsMenu.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace SummerGameProject.Src.GameStates
+{
+    class SettingsMenu : GameState
+    {
+        public override void Draw(GameTime gameTime, SpriteBatch spriteBatch)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Update(GameTime gameTime, KeyboardState keyboardState)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
