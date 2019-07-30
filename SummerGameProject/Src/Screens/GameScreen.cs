fc4f302164cfe469d920e33254b03a5672eb0fd3
Rob-Turff <robert_turff@yahoo.com>
2019-07-30 14:12:38 +0100

    added floor

--- SummerGameProject/Src/Screens/GameScreen.cs
+++ SummerGameProject/Src/Screens/GameScreen.cs
@@ -5,19 +5,22 @@ using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
+using SummerGameProject.Src.Components.Sprites;
 
 namespace SummerGameProject.Src.Screens
 {
     class GameScreen : Screen
     {
+
         public GameScreen(MainGame game, GraphicsDeviceManager graphics) : base(game, graphics)
         {
         }
 
         public override void LoadContent()
         {
-            Texture2D ground = Content.Load<Texture2D>("Game/GroundV1");
-            components.Add(ground);
+            Texture2D floorTexture = Content.Load<Texture2D>("Game/GroundV1");
+            Platform floor = new Platform(floorTexture, new Vector2(0, graphics.PreferredBackBufferHeight - floorTexture.Height), Color.White);
+            components.Add(floor);
         }
 
         public override void UnloadContent()
