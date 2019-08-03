145e160df2038930bb2a4ec98afcdddf75d1f19d
rxt711 <rxt711@cs.bham.ac.uk>
2019-08-03 10:55:55 +0100

    added player animations

--- SummerGameProject/Src/Components/Player/Player.cs
+++ SummerGameProject/Src/Components/Player/Player.cs
@@ -2,6 +2,7 @@
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using SummerGameProject.Src.Screens;
+using SummerGameProject.Src.Utilities;
 using System;
 
 namespace SummerGameProject.Src.Components.Player
@@ -10,15 +11,16 @@ namespace SummerGameProject.Src.Components.Player
     {
         private Screen screen;
         private string playerName;
-        private SpriteEffects flipTexture; // Probs move this into animation class when done
         private PlayerMovementHandler movementHandler;
+        private Animation animation;
+        private AnimationHandler animationHandler;
 
         public override Vector2 Position { get => movementHandler.Position; set => movementHandler.Position = value; }
 
         public Player(Vector2 position, Screen screen, string name)
         {
             this.screen = screen;
-            this.movementHandler = new PlayerMovementHandler(this, screen.Components);
+            this.movementHandler = new PlayerMovementHandler(this, screen.Components, animationHandler);
             this.Position = position;
             this.playerName = name;
         }
@@ -27,23 +29,20 @@ namespace SummerGameProject.Src.Components.Player
         public override void Update(GameTime gameTime)
         {
             movementHandler.Update(gameTime);
+            animationHandler.Update(gameTime);
         }
 
         public override void Draw(GameTime gameTime, SpriteBatch spriteBatch)
         {
-            if (movementHandler.IsPlayerMovingLeft)
-            {
-                spriteBatch.Draw(Texture, Position, null, Color.White, 0f, Vector2.Zero, Scale, SpriteEffects.FlipHorizontally, 0f);
-            }
-            else
-            {
-                spriteBatch.Draw(Texture, Position, null, Color.White, 0f, Vector2.Zero, Scale, SpriteEffects.None, 0f);
-            }
+            animationHandler.MovingLeft = movementHandler.IsPlayerMovingLeft;
+            animationHandler.Draw(spriteBatch);
         }
 
         public void LoadContent()
         {
-            Texture = screen.Content.Load<Texture2D>("Game/Player");
+            Texture = screen.Content.Load<Texture2D>("Game/WizardSpriteSheet");
+            animation = new Animation(7, 0.5f, Texture);
+            animationHandler = new AnimationHandler(animation, this);
         }
 
     }
